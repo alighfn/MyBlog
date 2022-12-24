7545d26062eb27c2cb3a61a4d59a77f43dc7d0b3
Ali Ghaffarinejad <73075902+alighfn@users.noreply.github.com>
2022-12-24 12:45:00 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Simple Blog Website using Flask backend and MongoDb as Database
---
+++ app.py
@@ -0,0 +1,30 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient('mongodb://localhost:27017')
+    app.db = client.myblog
+
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            entry_content = request.form.get('content')
+            formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert_one(
+                {"content": entry_content, "date": formatted_date, 'test': 'test_value'})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(
+                    entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("index.html", entries=entries_with_date)
+
+    return app
